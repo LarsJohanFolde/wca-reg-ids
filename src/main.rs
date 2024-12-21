3b7afdd19f9804ca45a9009a4cf5afd67bc05737
Lars Johan Folde <ljfolde@gmail.com>
2024-12-22 00:38:20 +0100

    Misc improvements

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,23 @@ use reqwest;
 use serde_json::{self};
 use std::env;
 mod country_names;
+use crate::person::Person;
 mod person;
 
 // TODO add information flag that prints competitioninfo such as whether reg is still open...
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    //  TODO: Sanitize input
     let args: Vec<String> = env::args().collect();
-    if args.len() == 2 {
-        list_competitors(args[1].to_string()).await?;
+    if args.len() != 2 {
+        println!(
+            "reg: Unexpected number of arguments. Expected 1 but got {}",
+            args.len() - 1
+        );
         std::process::exit(1);
     }
 
-    println!(
-        "reg: Unexpected number of arguments. Expected 1 but got {}",
-        args.len() - 1
-    );
+    list_competitors(args[1].to_string()).await?;
     Ok(())
 }
 
@@ -31,66 +31,56 @@ async fn list_competitors(competition_id: String) -> Result<(), Box<dyn std::err
     println!("Requesting data...");
     let resp = reqwest::get(url).await?;
 
-    if resp.status().is_success() {
-        let data: serde_json::Value = resp.json().await?;
-        let competition_name = data["name"].to_string().replace("\"", "");
-        println!("\n----------------------------------\n");
-        println!("Showing results for {}:\n", competition_name);
-        // Create and print persons
-        if let Some(persons) = data["persons"].as_array() {
-            let mut non_competing_persons: usize = 0;
-            let mut first_timers: u32 = 0;
-            for person in persons {
-                if format!("{}", person["registration"]).as_str() == "null" {
-                    non_competing_persons += 1;
-                    continue;
-                }
-
-                let person = person::new(
-                    person["registrantId"].to_string(),
-                    person["name"].to_string().replace("\"", ""),
-                    person["wcaId"].to_string().replace("\"", ""),
-                    country_names::common_name(&person["countryIso2"].as_str().unwrap())
-                        .to_string(),
-                    person["registration"]["isCompeting"].as_bool().unwrap(),
-                );
-
-                // Remove non-competing organizers and delegates
-                if !person.is_competing {
-                    non_competing_persons += 1;
-                    continue;
-                }
-                
-                let mut output: String = format!("{}: {}, {}", person.id, person.name, person.country_id);
-
-                // Mark and count first-time competitors
-                if person.wca_id == "null" {
-                    output.push_str(" (First-Timer)");
-                    first_timers += 1;
-                }
-
-                println!("{}", output);
-            }
-
-            // Count registered competitors by taking the amount of people minus the amount of
-            // people not registered.
-            let competitor_count: usize = persons.len() - non_competing_persons;
-
-            // Print a message if there are no competitors
-            if competitor_count <= 0 {
-                println!("No registrations for {}", competition_name);
-            }
-
-            println!(
-                "\nTotal registrations: {}/{}",
-                competitor_count, data["competitorLimit"]
-            );
-            println!("First-time competitors: {}", first_timers);
-        } else {
-            println!("'persons' field is not in the expected format or is missing.");
-        }
-    } else {
+    if !resp.status().is_success() {
         println!("Failed to fetch data: {}", resp.status());
+        std::process::exit(1);
     }
+
+    let data: serde_json::Value = resp
+        .json()
+        .await?;
+
+    let competition_name = data["name"]
+        .to_string()
+        .replace("\"", "");
+
+    println!("\n----------------------------------\n");
+    println!("Showing results for {}:\n", competition_name);
+
+    let registrants: Vec<Person> = data["persons"]
+        .as_array()
+        .unwrap_or(&vec![])
+        .iter()
+        .filter_map(Person::from)
+        .collect();
+
+    let first_timers = registrants
+        .iter()
+        .filter(|r| r.wca_id == "null")
+        .count();
+
+    let total_registrants = registrants
+        .iter()
+        .filter(|r| r.is_competing)
+        .count();
+
+    let competitor_limit = data["competitorLimit"]
+        .to_string()
+        .parse::<u16>()
+        .unwrap();
+
+    for person in registrants.iter().filter(|r| r.is_competing) {
+        println!(
+            "{}: {}, {} ({})",
+            person.id,
+            person.name,
+            person.country_id,
+            if person.wca_id == "null" { "First-timer" } else { &person.wca_id }
+        );
+    }
+
+    println!("\nTotal registations: {}/{}", total_registrants, competitor_limit);
+    println!("First-time competitors: {}", first_timers);
+
     Ok(())
 }
--- src/person.rs
+++ src/person.rs
@@ -1,25 +1,41 @@
+#![allow(unused)]
+use serde_json::Value;
+
+use crate::country_names;
+
 #[derive(Debug)]
 pub struct Person {
-    pub id: u16,
+    pub id: String,
     pub wca_id: String,
     pub name: String,
     pub country_id: String,
     pub is_competing: bool,
 }
 
-pub fn new(
-    id_as_string: String,
-    name: String,
-    wca_id: String,
-    country_id: String,
-    is_competing: bool,
-) -> Person {
-    Person {
-        id: id_as_string.parse().unwrap(),
-        name,
-        wca_id,
-        country_id,
-        is_competing,
+impl Person {
+    pub fn new(
+        id_as_string: String,
+        name: String,
+        wca_id: String,
+        country_id: String,
+        is_competing: bool,
+    ) -> Self {
+        Self {
+            id: id_as_string,
+            name,
+            wca_id,
+            country_id,
+            is_competing,
+        }
+    }
+
+    pub fn from(data: &Value) -> Option<Self> {
+        Some(Self {
+            id: data["registrantId"].to_string(),
+            name: data["name"].to_string().replace("\"", ""),
+            wca_id: data["wcaId"].to_string().replace("\"", ""),
+            country_id: country_names::common_name(data["countryIso2"].as_str().unwrap()).to_string(),
+            is_competing: data["registration"]["isCompeting"].as_bool().unwrap_or(false),
+        })
     }
 }
-
